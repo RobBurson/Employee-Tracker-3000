d4499dee6ea809c2e4bbc668eb14e0c34bf731ac
Rob <robburson07@gmail.com>
2021-12-11 11:48:21 -0600

    created db folder, tracker sql and seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+USE tracker;
+
+INSERT INTO Department (name)
+VALUES ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal');
+
+INSERT INTO Role
+    (title, salary, dep_id)
+VALUES 
+    ('Sales Manager', 100000, 1),
+    ('Sales Rep', 75000, 1),
+
+    ('Software Engineer', 130000, 2),
+    ('Structural Engineer', 125000, 2),
+
+    ('Accounts Manager', 145000, 3),
+    ('Accountant', 110000, 3),
+
+    ('Lead Legal Advisor', 165000, 4),
+    ('Lawyer', 155000, 4);
+
+INSERT INTO Employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Rob', 'Burson', 1, NULL),
+    ('Hunter', 'Hughes', 2, 1),
+    ('Paul', 'Diaz', 3, NULL),
+    ('Jessica', 'Diaz', 4, 3),
+    ('Chuck', 'Hunter', 5, NULL),
+    ('Jerry', 'Robertson', 6, 5),
+    ('Labaren', 'Holland', 7, NULL),
+    ('Hunter', 'Smith', 8, 7);
